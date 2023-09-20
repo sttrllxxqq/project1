f391e23c0ff1e777641cd2ff3b126c2e181d0f1a
жenya <110282058+sttrllxxqq@users.noreply.github.com>
2023-09-20 18:54:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class MyClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default MyClassComponent;
+
+import React, { Component } from 'react';
+
+class MyClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default MyClassComponent;
